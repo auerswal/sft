e30731704be25ecd6f4202b73d072a731d0fd83b
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2018-03-16 11:53:03 +0100

    add section.pl, a Perl version of section

--- README.md
+++ README.md
@@ -47,5 +47,6 @@ IPv6 ULA addresses using a deterministic algorithm based on [RFC 4193 section 3.
 * `priv_pass.py` - Calculate ROMMON priv password for (some) Cisco routers
 * `rfc-reader` - read IETF RFCs on GNU/Linux ([web page](https://www.unix-ag.uni-kl.de/~auerswal/rfc-reader/))
 * `section` - print all lines matching a regular expression, and the following indented section (inspired by the Cisco IOS output modifier of the same name)
+* `section.pl` - Perl version of the above `section` script, for use on systems without awk, such as some Cisco IOS XR versions
 * `total_video_duration` - print total run time of given video files (uses `ffprobe` from [FFmpeg](https://ffmpeg.org/))
 * `vevent_dump` - print a somewhat readable version of the essential informationen inside an iCalendar (.ics) file to STDOUT (best effort only, not even trying to parse the format...)
---
+++ section.pl
@@ -0,0 +1,34 @@
+#! /usr/bin/perl -w
+
+# section.pl - print line matching regexp with following indented section
+#
+# Copyright (C) 2018 by Erik Auerswald <auerswal@unix-ag.uni-kl.de>
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+use strict;
+
+if (scalar(@ARGV) == 0) { print "Usage: section PATTERN FILE...\n"; exit 1;}
+
+my $pat = $ARGV[0]; shift;
+my $in_sec = 0;
+my $ind = '';
+my $l_ind = '';
+my $l_mat = 0;
+
+while (<>) {
+	($l_ind) = (/^([ \t]*)/);
+	$l_ind =~ s/\t/        /g;
+	$l_mat = /$pat/ ? 1 : 0;
+	if ($l_mat || ($in_sec && length $l_ind > length $ind)) {
+		if (!$in_sec || (length $l_ind < length $ind)) { $ind = $l_ind; }
+		$in_sec = 1;
+		print;
+	} else {
+		$in_sec = 0;
+		$ind = '';
+	}
+}
