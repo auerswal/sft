7782e457f5c505cf7cdeb146bbfe09f5b46dcdb7
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2019-07-07 18:24:10 +0200

    section.py: add comment about Python 3 and UTF-8
    This script does not work around the Python 3 default UTF-8
    idiosyncrasies, but one can set the locale to C or POSIX to
    prevent the more obvious shortcomings.

--- section.py
+++ section.py
@@ -12,6 +12,8 @@
 
 # Please note that this program uses Python regular expressions which are
 # not compatible with POSIX extended regular expressions (ERE).
+# Please note that using this program with Python 3 with a non-POSIX
+# locale may result in UnicodeDecodeError with non-UTF-8 input.
 
 # Make coding more python3-ish
 from __future__ import (
