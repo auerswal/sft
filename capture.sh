bde94c1efff33f5089c334cb0a59939a22053ff8
Erik Auerswald <auerswal@unix-ag.uni-kl.de>
2024-01-24 20:46:25 +0100

    capture.sh: address ShellCheck complaints
    * SC2086 (info): Double quote to prevent globbing and word splitting.
      https://www.shellcheck.net/wiki/SC2086
    * SC2250 (style): Prefer putting braces around variable references
      even when not strictly required.
      https://www.shellcheck.net/wiki/SC2250

--- capture.sh
+++ capture.sh
@@ -2,7 +2,7 @@
 
 # Use tcpdump to capture traffic for a given time.
 #
-# Copyright (C) 2016-2018 Erik Auerswald <auerswald@unix-ag.uni-kl.de>
+# Copyright (C) 2016-2024 Erik Auerswald <auerswald@unix-ag.uni-kl.de>
 #
 # Copying and distribution of this file, with or without modification,
 # are permitted in any medium without royalty provided the copyright
@@ -16,18 +16,18 @@ CF='capture.pcap'
 DURATION="1m"
 IF="$1"
 
-test -z "$1" && { echo usage: $0 INTERFACE; exit 1; }
+test -z "$1" && { echo usage: "$0" INTERFACE; exit 1; }
 
-rm -fv "$CF"
+rm -fv "${CF}"
 # XXX "tcpdump -G60 -W1" does not stop if interface down
 # XXX tcpdump needs duration in seconds, just as POSIX sleep
-# tcpdump -i "$IF" -G"$DURATION" -W1 -w "$CF" -v
+# tcpdump -i "${IF}" -G"${DURATION}" -W1 -w "${CF}" -v
 # Dumpcap example:
-# dumpcap -i "$IF" -a duration:"$DURATION" -w "$CF"
+# dumpcap -i "${IF}" -a duration:"${DURATION}" -w "${CF}"
 # Since tcpdump -G is not reliable, and dumpcap is not available, start
 # a tcpdump process and kill it after the given duration:
-tcpdump -w "$CF" -i "$IF" -v &
+tcpdump -w "${CF}" -i "${IF}" -v &
 DUMPPID=$!
-sleep "$DURATION"
-kill "$DUMPPID"
+sleep "${DURATION}"
+kill "${DUMPPID}"
 wait
